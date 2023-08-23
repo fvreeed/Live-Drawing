05ca1408290aa9e264a1e1ef0a90238841b7dbb0
fvreeed <novruzovfar1d@mail.ru>
2023-08-23 21:29:16 +0300

    Multithread

---
+++ app/src/main/java/com/example/livedrawing/LiveDrawingActivity.java
@@ -0,0 +1,39 @@
+package com.example.livedrawing;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
+
+public class LiveDrawingActivity extends Activity {
+
+    private LiveDrawingView mLiveDrawingView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        mLiveDrawingView = new LiveDrawingView(this, size.x, size.y);
+        setContentView(mLiveDrawingView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mLiveDrawingView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mLiveDrawingView.pause();
+    }
+}
